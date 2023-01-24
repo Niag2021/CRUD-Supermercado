00a9801e352bc9b206b7b5ce811d4d97c0d9fabf
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2023-01-24 16:04:30 -0300

    Creacion carpeta servidor, archivo index.js y BD

---
+++ db.sql
@@ -0,0 +1,15 @@
+-- create database supermercado;
+-- use supermercado;
+
+-- Tabla Bebidas
+/*
+CREATE TABLE bebidas ( 
+    id INTEGER PRIMARY KEY auto_increment, 
+    nombre VARCHAR(200) NOT NULL,
+    descripcion VARCHAR(300) NOT NULL,
+    litros INTEGER(10) NOT NULL,
+    tipo BOOLEAN NOT NULL DEFAULT 0,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+*/
